70eace67e42f0e2f1a9be3aabb607da7180fe7ae
John Alexander Quintero <quinteroberrio@gmail.com>
2020-01-19 20:36:24 -0500

    Se implementa charts en home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 
-
-
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//chart.js
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  PieChart = [];
   constructor() { }
 
   ngOnInit() {
-  }
 
+
+// pie chart:
+this.PieChart = new Chart('pieChart', {
+  type: 'doughnut',
+  data: {
+    labels: ["Completados", "Faltantes"],
+    datasets: [{
+      label: 'Completados',
+      data: [9, 7],
+      backgroundColor: [
+        'rgba(123, 31, 162, 1)',
+        'rgba(255, 255, 255)'
+      ],
+      borderColor: [
+        'rgba(123, 31, 162, 1)',
+        'rgba(123, 31, 162, 1)'
+      ],
+      borderWidth: 5
+    }]
+  },
+  options: {
+    title: {
+      text: "Relación del total del llamamientos en la Estaca",
+      display: true,
+      fontColor: 'rgb(255, 255, 255)'
+    },legend: {
+      display: true,
+      labels: {
+        fontColor: 'rgb(255, 255, 255)'
+      }
+    }
+  }
+});
+
+}
 }
